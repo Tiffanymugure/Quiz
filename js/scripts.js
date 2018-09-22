1ac131d31c2048223622ef66b9f819caa051f4da
tiffany mugure <amiratiffany@gmail.com>
2018-09-22 14:54:37 +0300

    initial gh-pages

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+
+function tabulateAnswers() {
+  var c1score = 0;
+  var c2score = 0;
+  var c3score = 0;
+  var c4score = 0;
+  var c5score = 0;
+  var choices = document.getElementsByTagName('input');
+  for (i=0; i<choices.length; i++) {
+    if (choices[i].checked) {
+      if (choices[i].value == 'c1') {
+        c1score = c1score + 1;
+      }
+      if (choices[i].value == 'c2') {
+        c2score = c2score + 1;
+      }
+      if (choices[i].value == 'c3') {
+        c3score = c3score + 1;
+      }
+      if (choices[i].value == 'c4') {
+        c4score = c4score + 1;
+      }
+      if (choices[i].value == 'c5') {
+        c5score = c4score + 1;
+      }
+
+    }
+  }
+
+  var maxscore  = Math.max(c1score,c2score,c3score,c4score,c5score);
+
+  var answerbox = document.getElementById('answer');
+  if (c1score == maxscore) {
+    answerbox.innerHTML = alert("Excellent.");
+  }
+  if (c2score == maxscore) {
+    answerbox.innerHTML = alert("Good");
+  }
+  if (c3score == maxscore) {
+    answerbox.innerHTML = alert("Average.");
+  }
+  if (c4score == maxscore) {
+    answerbox.innerHTML = alert("Below Average.");
+  }
+}
+  if (c5score == maxscore) {
+    answerbox.innerHTML = alert("Poor.");
+}
+
+function resetAnswer() {
+  var answerbox = document.getElementById('answer');
+  answerbox.innerHTML = "Your result will show up here!";
+}
